4c4282c77128ea4d2d3bca5ca6ace9ba76904b3d
kzeeyang <2402989406@qq.com>
2020-11-24 22:09:41 +0800

    add

--- lib/common/utils/timeUtils.dart
+++ lib/common/utils/timeUtils.dart
@@ -1,5 +1,12 @@
 import 'package:common_utils/common_utils.dart';
 
+const String JUST_NOW = "刚刚";
+const String MINITE_AGO = "分钟前";
+const String HOUR_AGO = "小时前";
+const String DAY_AGO = "天前";
+const String THIS_YEAR_FORMAT = "MM/dd";
+const String OVER_YEAR_FORMAT = "yyyy/MM/dd";
+
 String timeUtils(DateTime date) {
   DateTime utcDate = DateTime.now();
   if (!utcDate.isUtc) {
@@ -12,20 +19,24 @@ String timeUtils(DateTime date) {
       utcDate = utcDate.subtract(date.timeZoneOffset);
     }
   }
+  // print('utcDate: $utcDate, date: $date');
+
+  // print(
+  //     'utcDate: ${utcDate.millisecondsSinceEpoch}, date: ${date.millisecondsSinceEpoch}');
 
   if (date.year != utcDate.year) {
-    return DateUtil.formatDate(date, format: "yyyy/MM/dd");
+    return DateUtil.formatDate(date, format: OVER_YEAR_FORMAT);
   }
   Duration duration = utcDate.difference(date);
   if (duration.inDays > 9) {
-    return DateUtil.formatDate(date, format: "MM/dd");
+    return DateUtil.formatDate(date, format: THIS_YEAR_FORMAT);
   } else if (duration.inDays > 0) {
-    return duration.inDays.toString() + "天前";
+    return duration.inDays.toString() + DAY_AGO;
   } else if (duration.inHours > 0) {
-    return duration.inHours.toString() + "小时前";
+    return duration.inHours.toString() + HOUR_AGO;
   } else if (duration.inMinutes > 0) {
-    return duration.inMinutes.toString() + "分钟前";
+    return duration.inMinutes.toString() + MINITE_AGO;
   } else {
-    return "刚刚";
+    return JUST_NOW;
   }
 }
