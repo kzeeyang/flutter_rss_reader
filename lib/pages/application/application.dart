cbd0139791e8617186b36c2e7cda748738e601d8
kzeeyang <2402989406@qq.com>
2020-12-01 21:42:55 +0800

    add

--- lib/pages/application/application.dart
+++ lib/pages/application/application.dart
@@ -4,9 +4,11 @@ import 'package:flutter_rss_reader/common/router/router.gr.dart';
 import 'package:flutter_rss_reader/common/utils/screen.dart';
 import 'package:flutter_rss_reader/common/utils/utils.dart';
 import 'package:flutter_rss_reader/common/values/colors.dart';
+import 'package:flutter_rss_reader/common/widgets/widgets.dart';
 import 'package:flutter_rss_reader/global.dart';
 import 'package:flutter_rss_reader/pages/application/bodyWidget.dart';
 import 'package:flutter_rss_reader/pages/application/floatingActionButton.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class ApplicationPage extends StatefulWidget {
@@ -15,6 +17,8 @@ class ApplicationPage extends StatefulWidget {
 }
 
 class _ApplicationPageState extends State<ApplicationPage> {
+  DateTime _lastTime;
+
   @override
   void initState() {
     super.initState();
@@ -61,37 +65,52 @@ class _ApplicationPageState extends State<ApplicationPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
-      body: Global.appState.showCategory == null
-          ? Container(
-              color: AppColors.primaryGreyBackground,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text('尚未添加分类'),
-                    ],
-                  ),
-                ],
-              ),
-            )
-          : Global.appState.showCategory.rssSettings.length == 0
-              ? Container(
-                  color: AppColors.primaryGreyBackground,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text('分类下尚未添加RSS'),
-                        ],
-                      ),
-                    ],
-                  ),
-                )
-              : BodyWidget(),
+      body: WillPopScope(
+        onWillPop: () async {
+          if (_lastTime == null ||
+              DateTime.now().difference(_lastTime) > Duration(seconds: 1)) {
+            //两次点击间隔超过1s重新计时
+            _lastTime = DateTime.now();
+            toastInfo(
+              msg: "再按一次退出",
+              toastGravity: ToastGravity.BOTTOM,
+            );
+            return false;
+          }
+          return true;
+        },
+        child: Global.appState.showCategory == null
+            ? Container(
+                color: AppColors.primaryGreyBackground,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('尚未添加分类'),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            : Global.appState.showCategory.rssSettings.length == 0
+                ? Container(
+                    color: AppColors.primaryGreyBackground,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text('分类下尚未添加RSS'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                : BodyWidget(),
+      ),
       floatingActionButton: Global.appState.showCategory == null
           ? Container()
           : AnimationFloatingButton(),
